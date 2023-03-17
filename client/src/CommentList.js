062720f6de2c0719213a5f92727a573e0813e8e1
Zhou-Duo <xzhouaz@connect.ust.hk>
2023-03-17 04:26:06 -0400

    render comments on status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,17 @@ import React from 'react';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === 'accepted') {
+      content = comment.content;
+    }
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected.';
+    }
+    if (comment.status === 'pending') {
+      content = 'This comment has been waiting to be moderated.';
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
